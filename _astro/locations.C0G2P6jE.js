2f20b2e46b325e21c7118bd2700b39bedaf2eebf
cristalcorp <cristal@cristalcorp.com>
2025-06-28 19:51:27 +0200

    Updates

--- _astro/locations.C0G2P6jE.js
+++
@@ -1,9 +0,0 @@
-var u={exports:{}},e={};/**
- * @license React
- * react-jsx-runtime.production.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var i;function v(){if(i)return e;i=1;var a=Symbol.for("react.transitional.element"),R=Symbol.for("react.fragment");function o(l,r,t){var s=null;if(t!==void 0&&(s=""+t),r.key!==void 0&&(s=""+r.key),"key"in r){t={};for(var n in r)n!=="key"&&(t[n]=r[n])}else t=r;return r=t.ref,{$$typeof:a,type:l,key:s,ref:r!==void 0?r:null,props:t}}return e.Fragment=R,e.jsx=o,e.jsxs=o,e}var x;function d(){return x||(x=1,u.exports=v()),u.exports}var _=d();const p=[[48,81783,2,40700],[48,88584,2,34505],[48,84927,2,35691]],E=[[37.9974647,23.7368407]],c=[[52.49483162160716,13.446911212224357]],j={Paris:p,Athenes:E,Berlin:c};export{_ as j,j as l};
